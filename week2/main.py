0f2e4eac6d2d0aa679b33f7f166a15ace98ec42d
PerThoyra <per.thoyra@outlook.com>
2026-04-29 12:26:37 +0200

    new csv file, input handling changes

--- week2/main.py
+++ week2/main.py
@@ -8,35 +8,38 @@ def clear_screen() -> None:
         _ = system('cls')
     else:
         _ = system('clear')
+        
+def handle_selection(choice: int, menuoptions: dict) -> bool:
+    match choice:
+        case 0:
+            print("Terminating program...")
+            return False
+        case 1 | 2:
+            print("\n")
+            print(menuoptions[choice])
+            print("\n")
+            sleep(2)
+
+    # it is a bit quick, so the resulkt is not visible long enough
+    _ = input()
+    return True
 
 def run(mymenu: menu, menuoptions: dict) -> None:
     
     while True:
         clear_screen()
-        
         mymenu.draw_menu()
-        
         selected = mymenu.get_input()
-
-        match selected:
-            case 0:
-                print("Terminating program...")
-                break
-            case 1 | 2:
-                print("\n")
-                print(menuoptions[selected])
-                print("\n")
-                sleep(2)
-
-        # it is a bit quick, so the resulkt is not visible long enough
-        _ = input()
+        
+        if not handle_selection(selected, menuoptions):
+            break
 
 def main():
     thisdict = {
         1: "Select option 1.",
         2: "Select option 2.",
         0: "Exit"
-        }
+    }
     
     myMenu = menu.MyMenu(thisdict)
        
